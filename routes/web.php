080e47be8b7bd32ad069b168a0992e90676211ca
YuNandar118 <yunandar15502@gmail.com>
2021-01-15 07:18:34 -0800

    Second added

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,25 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Auth::routes();
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/',function(){
-    return view('admin');
-});
-Route::get('/leave/changepassword',function(){
-    return view('changepassword');
-});
+//  Route::get('/',function(){
+//      return view('admin');
+// });
+// Route::get('/leave/changepassword',function(){
+//     return view('changepassword');
+// });
+
+// Route::get('/department',[DepartmentController::class,'index']);
+// Route::get('/department/create',[DepartmentController::class,'create']);
+// Route::post('/department',[DepartmentController::class,'store']);
+// Route::get('/department/{id}/edit',[DepartmentController::class,'edit']);
+// Route::patch('/department/{id}/edit',[DepartmentController::class,'update']);
+// Route::delete('/department/{departments}',[DepartmentController::class,'destroy']);
+
+
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
